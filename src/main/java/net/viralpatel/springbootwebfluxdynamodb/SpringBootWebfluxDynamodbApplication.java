24f149be5b640476e6b67377e4a3ddfe6eac4710
Viral Patel <174647+viralpatel@users.noreply.github.com>
2019-12-23 19:45:22 +1100

    Initialize

---
+++ src/main/java/net/viralpatel/springbootwebfluxdynamodb/SpringBootWebfluxDynamodbApplication.java
@@ -0,0 +1,13 @@
+package net.viralpatel.springbootwebfluxdynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxDynamodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebfluxDynamodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/viralpatel/springbootwebfluxdynamodb/SpringBootWebfluxDynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package net.viralpatel.springbootwebfluxdynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxDynamodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
